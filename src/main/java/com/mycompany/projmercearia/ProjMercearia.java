def59ae349f3861abd843cea55065980cd29bd1c
ANA <anaclaraoliveira1156@gmail.com>
2026-06-18 18:38:44 -0300

    Configuração inicial do pom.xml e dependências

---
+++ src/main/java/com/mycompany/projmercearia/ProjMercearia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projmercearia;
+
+/**
+ *
+ * @author anacl
+ */
+public class ProjMercearia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
