c4f8f43decf3b8a9c6e1fe386b205ef8f9180d9f
ANA <anaclaraoliveira1156@gmail.com>
2026-06-27 23:42:35 -0300

    Protótipo de Login

---
+++ ProjMercearia/src/main/java/com/mycompany/projmercearia/ModelMercearia.java
@@ -0,0 +1,54 @@
+package com.mycompany.projmercearia;
+public class Mercadoria {
+
+    private int id;
+    private String codigoIdentificador;
+    private String nome;
+    private String descricao;
+    private String categoria;
+
+    private double precoCompra;
+    private double precoVenda;
+
+    private int qtdEstoque;
+
+    private String fornecedor;
+
+
+    public Mercadoria(
+            int id,
+            String codigo,
+            String nome,
+            double precoVenda,
+            int qtdEstoque){
+
+        this.id=id;
+        this.codigoIdentificador=codigo;
+        this.nome=nome;
+        this.precoVenda=precoVenda;
+        this.qtdEstoque=qtdEstoque;
+    }
+
+
+    public boolean decrementarEstoque(int qtd){
+
+        if(qtdEstoque >= qtd){
+
+            qtdEstoque -= qtd;
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    public String getCodigo(){
+        return codigoIdentificador;
+    }
+
+
+    public double getPrecoVenda(){
+        return precoVenda;
+    }
+
+}
