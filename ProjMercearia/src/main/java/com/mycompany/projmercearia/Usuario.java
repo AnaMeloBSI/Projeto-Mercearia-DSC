7fd8a57f9e3e6798ce3b066ff3aa0cf06cb6d683
MaisaFerreira <maisapinheiroferreira1@gmail.com>
2026-06-30 15:05:34 -0300

    Adicionando Usuario.java

---
+++ ProjMercearia/src/main/java/com/mycompany/projmercearia/Usuario.java
@@ -0,0 +1,24 @@
+package com.mycompany.projmercearia;
+
+public class Usuario {
+    private String nome;
+    private String login;
+    private String senha;
+    private String perfil; // "Administrador" ou "Operador" [cite: 89]
+
+    public Usuario(String nome, String login, String senha, String perfil) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public String getPerfil() { return perfil; }
+    public void setPerfil(String perfil) { this.perfil = perfil; }
+}
