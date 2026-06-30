e901e2a75ac3c5910a7c1be2f9fd4b41b5afc211
MaisaFerreira <maisapinheiroferreira1@gmail.com>
2026-06-30 16:05:59 -0300

    update AcessoController.java

--- ProjMercearia/src/main/java/com/mycompany/projmercearia/AcessoController.java
+++ ProjMercearia/src/main/java/com/mycompany/projmercearia/AcessoController.java
@@ -1,37 +1,20 @@
 package com.mycompany.projmercearia;
 
-import java.util.ArrayList;
-
 public class AcessoController {
-    private ArrayList<Usuario> usuarios = new ArrayList<>();
-    private Usuario usuarioLogado = null;
-    private String horarioEntrada = "";
-    private String horarioSaida = "";
-
-    public boolean cadastrarUsuario(String nome, String login, String senha, String perfil) {
-        Usuario novo = new Usuario(nome, login, senha, perfil);
-        usuarios.add(novo);
-        return true;
-    }
+    private String usuarioLogado;
+    private String horarioEntrada;
+    private String horarioSaida;
 
     public boolean efetuarLogin(String login, String senha) {
-        for (Usuario u : usuarios) {
-            if (u.getLogin().equals(login) && u.getSenha().equals(senha)) {
-                usuarioLogado = u;
-                this.horarioEntrada = java.time.LocalDateTime.now().toString(); 
-                return true;
-            }
-        }
-        return false;
+        this.usuarioLogado = login;
+        this.horarioEntrada = java.time.LocalDateTime.now().toString(); 
+        return true;
     }
-
     public void efetuarLogout() {
-        if (usuarioLogado != null) {
-            this.horarioSaida = java.time.LocalDateTime.now().toString(); 
-            System.out.println("LOGOUT: " + usuarioLogado.getNome() + " saiu às " + horarioSaida);
-            usuarioLogado = null;
-        }
+        this.horarioSaida = java.time.LocalDateTime.now().toString(); 
+        System.out.println("Usuário " + usuarioLogado + " entrou às " + horarioEntrada + " e saiu às " + horarioSaida);
+        
+       
+        this.usuarioLogado = null;
     }
-
-    public Usuario getUsuarioLogado() { return usuarioLogado; }
-}
+} 
