920c064802ca10b786814f25211ea03596372835
MaisaFerreira <maisapinheiroferreira1@gmail.com>
2026-06-30 13:41:47 -0300

    Adicionando ItemVenda.java

---
+++ ProjMercearia/src/main/java/com/mycompany/projmercearia/ItemVenda.java
@@ -0,0 +1,16 @@
+package com.mycompany.projmercearia;
+
+public class ItemVenda {
+
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemVenda(int quantidade, double precoUnitario) {
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double calcularSubtotal() {
+        return quantidade * precoUnitario;
+    }
+}
